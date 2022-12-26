5b0e8941a912564434d79e23df90b7131e34b08e
Ben Plunkert <>
2022-12-25 19:10:32 -0500

    comparison stuff

--- app.py
+++ app.py
@@ -60,29 +60,34 @@ def prompt_comparison():
     comparison = compare_prompts(first_prompt, second_prompt, test)
     if comparison is True:
       better_results["first_prompt"] += 1
+      reason = qualitative_comparison(first_prompt, second_prompt, test)
     elif comparison is False:
       better_results["second_prompt"] += 1
+      reason = qualitative_comparison(second_prompt, first_prompt, test)
 
-    if better_results['first_prompt'] > better_results['second_prompt']:
-      return f"After {iterations}, first prompt is better ({better_results['first_prompt'] / iterations * 100}%)"
-    elif better_results['first_prompt'] < better_results['second_prompt']:
-      return f"After {iterations}, second prompt is better ({better_results['second_prompt'] / iterations * 100}%)"
-    else:
-      return f"After {iterations}, prompts are tied"
+
+  if better_results['first_prompt'] > better_results['second_prompt']:
+    return f"After {iterations}, first prompt is better ({better_results['first_prompt'] / iterations * 100}%) because {reason}. More suggestions: {recommend_more_prompts(first_prompt, test, reason)}"
+  elif better_results['first_prompt'] < better_results['second_prompt']:
+    return f"After {iterations}, second prompt is better ({better_results['second_prompt'] / iterations * 100}%) because {reason}. More suggestions: {recommend_more_prompts(second_prompt, test, reason)}"
+  else:
+    return f"After {iterations}, prompts are tied"
 
 @app.route('/prompt', methods=['POST'])
 def prompt():
   prompt = request.form['prompt']
-  response = openai.Completion.create(
-    model='text-davinci-003',
-    prompt=prompt,
-    temperature=TEMPERATURE,
-    max_tokens=MAX_TOKENS,
-    top_p=TOP_P,
-    frequency_penalty=FREQUENCY_PENALTY,
-    presence_penalty=PRESENCE_PENALTY,
-    stop=STOP
-  ).choices[0].text
+  if prompt.startswith('DEPLOY'):
+  else:
+    response = openai.Completion.create(
+      model='text-davinci-003',
+      prompt=prompt,
+      temperature=TEMPERATURE,
+      max_tokens=MAX_TOKENS,
+      top_p=TOP_P,
+      frequency_penalty=FREQUENCY_PENALTY,
+      presence_penalty=PRESENCE_PENALTY,
+      stop=STOP
+    ).choices[0].text
   print(f"Got response: {response}", file=sys.stderr)
   put_context('chats', prompt + "\n" + response)
   return response + "\n" + get_context('chats')
@@ -191,6 +196,68 @@ def compare_prompts(first_prompt, second_prompt, test, iterations = 1):
     return False
   else:
     return None
+
+def qualitative_comparison(better_prompt, worse_prompt, test):
+  """ Given a better prompt and a worse prompt, return a qualitative comparison of why the better prompt is better"""
+  print("Qualitative comparison", file=sys.stderr)
+  prompt = "We want to write a prompt that generates results that consistently satisfy a test. Given a better prompt, a worse prompt, and a test, explain what differentiates the better prompt from the worse prompt. (Phrase like this: 'A better prompt should...')"
+  prompt += "\nBetter prompt:\n" + better_prompt
+  prompt += "\nWorse prompt:\n" + worse_prompt
+  prompt += "\nTest:\n" + test
+  response = openai.Completion.create(
+    model='text-davinci-003',
+    prompt=prompt,
+    temperature=TEMPERATURE,
+    max_tokens=MAX_TOKENS,
+    top_p=TOP_P,
+    frequency_penalty=FREQUENCY_PENALTY,
+    presence_penalty=PRESENCE_PENALTY,
+    stop=STOP
+  ).choices[0].text
+  return response
+
+def recommend_more_prompts(prompt, test, guidelines = "", n = 1):
+  """ Given a prompt, a test, and optionally some guidelines, return a prompt that is similar to the given prompt, but that is different in some way. """
+  print("Recommend more comparisons", file=sys.stderr)
+  prompt += "Prompt:\n" + prompt
+  prompt += "Test:\n" + test
+  prompt += "Guidelines:\n" + guidelines
+  prompt += "Return a prompt that is similar to the given prompt, but that is different in some way:\n"
+  response = openai.Completion.create(
+    model='text-davinci-003',
+    prompt=prompt,
+    temperature=0.6,
+    max_tokens=MAX_TOKENS,
+    top_p=TOP_P,
+    frequency_penalty=FREQUENCY_PENALTY,
+    presence_penalty=PRESENCE_PENALTY,
+    stop=':'
+  ).choices[0].text
+  return response
+
+def deploy_code(current_code, change_request):
+    # Use the OpenAI API to request a code update
+    response = openai.Code.create(
+        model='code-davinci-002',
+        prompt=f"{current_code}\n{change_request}",
+        temperature=0.5,
+        max_tokens=1024
+    ).choices[0].code
+
+    # Extract the updated code from the response
+    updated_code = response['choices'][0]['text']
+
+    # Replace the current code with the updated code in memory
+    exec(updated_code, globals())
+
+    # Write the updated code to a file in the current directory
+    with open('updated_code.py', 'w') as f:
+        f.write(updated_code)
+
+
+    # Return a message to confirm that the code has been updated
+    return "Code updated successfully"
+
 
 if __name__ == '__main__':
   app.run(host='0.0.0.0', port=8080, debug=True)
